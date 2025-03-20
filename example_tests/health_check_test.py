7077357258781f8222a4c8a9700a9675e7c97183
jonas.puksta.sensmetry <jonas.puksta@sensmetry.com>
2025-03-20 17:00:39 +0200

    update checks which use historical data to use simpler API

--- example_tests/health_check_test.py
+++ example_tests/health_check_test.py
@@ -1,51 +1,114 @@
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
-from typing import Any, AsyncIterable, Sequence, Type, assert_never
+import asyncio
+from datetime import datetime, timedelta
+from typing import Any, AsyncIterable, Coroutine, Iterable
 from opensearchpy import AsyncOpenSearch
-from pydantic import BaseModel
 import pytest
 from opentelemetry import trace
 from opentelemetry.util import types
-from opentelemetry.trace.span import Span
 
+from python_opentelemetry_access import otlpjson, proxy
+from python_opentelemetry_access.base import Span
 from python_opentelemetry_access.proxy.opensearch.ss4o import OpenSearchSS40Proxy
-
-tracer = trace.get_tracer("pytest-opentelemetry")
 
 
 type NestedDict = dict[str, NestedDict | types.AttributeValue]
 
 
-def flatten_internal(d: NestedDict, name: str) -> dict[str, types.AttributeValue]:
-    result: dict[str, types.AttributeValue] = {}
-    if name:
-        name += "."
-    for key, value in d.items():
-        cur_name = name + key
-        match value:
-            case int() | str() | float() | bool() | Sequence():
-                result[cur_name] = value
-            case dict():
-                result |= flatten_internal(value, cur_name)
-            case unreachable:
-                assert_never(unreachable)
-    return result
+def _get_fields_attr_name(data_name: str) -> str:
+    return f"data_{data_name}_fields"
 
 
-def flatten(d: NestedDict) -> dict[str, types.AttributeValue]:
-    return flatten_internal(d, "")
+# THESE TWO functions should probably be the new API, at least for the simplest cases
+# THOUGH with this you don't get span length. Might include span length in the result?
+def save_data(data_name: str, data: dict[str, types.AttributeValue]) -> None:
+    cur_span = trace.get_current_span()
+    cur_span.set_attribute(_get_fields_attr_name(data_name), list(data.keys()))
+    trace.get_current_span().set_attributes(data)
 
 
-def set_span_attributes(span: Span, data: BaseModel) -> None:
-    span.set_attributes(flatten_internal(data.model_dump(), ""))
+async def load_data_from_name_async(
+    proxy: proxy.Proxy, data_name: str, max_data_age: timedelta
+) -> AsyncIterable[dict[str, types.AttributeValue]]:
+    try:
+        now = datetime.now()
+        fields_attr_name = _get_fields_attr_name(data_name)
+        async for spanCollection in proxy.query_spans_async(
+            from_time=now - max_data_age,
+            to_time=now,
+            span_attributes={fields_attr_name: None},
+        ):
+            for _resource, _scope, span in spanCollection.iter_spans():
+                # fields = [
+                #     expect_str(field)
+                #     for field in expect_list(span.otlp_attributes[fields_attr_name])
+                # ]
+                fields = span.otlp_attributes[fields_attr_name]
+                yield {field: span.otlp_attributes[field] for field in fields}  # type: ignore
+    finally:
+        await proxy.aclose()
 
 
-async def get_data[T: BaseModel](
-    data_type: Type[T],
-    span_name: str,
-    from_time: datetime | None,
-    to_time: datetime | None,
-) -> AsyncIterable[T]:
+async def load_span_from_fields_async(
+    proxy: proxy.Proxy, data_fields: list[str], max_data_age: timedelta
+) -> AsyncIterable[Span]:
+    try:
+        now = datetime.now()
+        async for spanCollection in proxy.query_spans_async(
+            from_time=now - max_data_age,
+            to_time=now,
+            span_attributes={field: None for field in data_fields},
+        ):
+            for _resource, _scope, span in spanCollection.iter_spans():
+                yield span
+    finally:
+        await proxy.aclose()
+
+
+async def load_span_from_span_name_async(
+    proxy: proxy.Proxy, span_name: str, max_data_age: timedelta
+) -> AsyncIterable[Span]:
+    try:
+        now = datetime.now()
+        async for spanCollection in proxy.query_spans_async(
+            from_time=now - max_data_age, to_time=now, span_name=span_name
+        ):
+            for _resource, _scope, span in spanCollection.iter_spans():
+                yield span
+    finally:
+        await proxy.aclose()
+
+
+async def async_list[T](
+    async_iterable: AsyncIterable[T],
+) -> Iterable[T]:
+    # TODO: would be nice to return the values as they become available, not accumulate them all first
+    results: list[T] = []
+    async for data in async_iterable:
+        results.append(data)
+    return results
+
+
+def async_to_sync_iterable[T](
+    async_iterable: AsyncIterable[T],
+) -> Iterable[T]:
+    return asyncio.run(async_list(async_iterable))
+
+
+def load_data_sync(
+    proxy: proxy.Proxy, data_name: str, max_data_age: timedelta
+) -> Iterable[dict[str, types.AttributeValue]]:
+    return async_to_sync_iterable(
+        load_data_from_name_async(proxy, data_name, max_data_age)
+    )
+    # return asyncio.run(_load_data_internal(proxy, data_name, max_data_age))
+
+
+def get_mock_proxy(file: str) -> proxy.Proxy:
+    with open(file, "r") as f:
+        return proxy.MockProxy(otlpjson.load(f))
+
+
+def get_opensearch_proxy() -> proxy.Proxy:
     opensearch_params: dict[str, Any] = {}
 
     # if osuser is not None and ospass is not None:
@@ -60,37 +123,72 @@ async def get_data[T: BaseModel](
         use_ssl=True,
         **opensearch_params,
     )
-    proxy = OpenSearchSS40Proxy(client)
-    try:
-        async for spanCollection in proxy.query_spans_async(from_time, to_time):
-            for resource, scope, span in spanCollection.iter_spans():
-                if span.otlp_name == span_name:
-                    # TODO: unflatten the data
-                    yield data_type.model_validate(span.otlp_attributes)
-    finally:
-        await client.close()
-
-
-class Data(BaseModel):
-    foo: int
-    bar: list[str]
+    return OpenSearchSS40Proxy(client)
 
 
 INFO_DUMP_SPAN_NAME = "INFO_DUMP"
 
 
+# def test_bla() -> None:
+#     with tracer.start_as_current_span("HELLO_SPAN") as span:
+#         span.set_attribute("hello", [1, 2, 3])
+
+
 def test_generate_data() -> None:
-    with tracer.start_as_current_span(INFO_DUMP_SPAN_NAME) as info_dump_span:
-        data = Data(foo=5, bar=["a", "bb", "ccc"])
-        set_span_attributes(info_dump_span, data)
+    save_data(data_name="my_data", data={"results": [1.0, 2.0, 2.0]})
+
+
+def test_get_requests_sync() -> None:
+    sum_sum = 0.0
+    sum_count = 0
+    for data in load_data_sync(
+        proxy=get_mock_proxy("example_tests/test_spans.json"),
+        data_name="my_data",
+        max_data_age=timedelta(weeks=4),
+    ):
+        sum_sum += sum(data["results"])
+        sum_count += 1
+    assert sum_sum / sum_count < 2.0
 
 
 @pytest.mark.asyncio
-async def test_read_data() -> None:
-    async for data in get_data(
-        data_type=Data,
-        span_name=INFO_DUMP_SPAN_NAME,
-        from_time=datetime.now() + relativedelta(months=-1),
-        to_time=datetime.now(),
+async def test_get_requests_async() -> None:
+    sum_sum = 0.0
+    sum_count = 0
+    async for data in load_data_from_name_async(
+        proxy=get_mock_proxy("example_tests/test_spans.json"),
+        data_name="my_data",
+        max_data_age=timedelta(weeks=4),
     ):
-        assert data.foo < 5
+        sum_sum += sum(data["results"])
+        sum_count += 1
+    assert sum_sum / sum_count < 2.0
+
+
+# # TODO: make this function unnecessary. proxy.query_spans_async should be able to do all the necessary filtering by itself
+# async def get_spans_with_data(
+#     proxy: proxy.Proxy,
+#     required_attributes: list[str],
+#     from_time: datetime | None,
+#     to_time: datetime | None,
+# ) -> AsyncIterable[base.Span]:
+#     async for spanCollection in proxy.query_spans_async(from_time, to_time):
+#         for _resource, _scope, span in spanCollection.iter_spans():
+#             # TODO: filter by these
+#             if all(attr in span.otlp_attributes for attr in required_attributes):
+#                 yield span
+
+
+# @pytest.mark.asyncio
+# async def test_get_requests() -> None:
+#     proxy = get_opensearch_proxy()
+#     try:
+#         async for span in get_spans_with_data(
+#             proxy=proxy,
+#             required_attributes=["http.status_code", "http.url", "http.method"],
+#             from_time=datetime.now() + relativedelta(months=-1),
+#             to_time=datetime.now(),
+#         ):
+#             assert span.otlp_attributes["http.status_code"] == 200
+#     finally:
+#         await proxy.aclose()
